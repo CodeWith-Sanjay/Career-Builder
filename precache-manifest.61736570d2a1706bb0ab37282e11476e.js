a88cadbeda0c04aefdfd58df8ee4d92209a2100c
CodeWith-Sanjay <sanjayganesan016@gmail.com>
2025-05-17 13:55:39 +0530

    Updates

---
+++ precache-manifest.61736570d2a1706bb0ab37282e11476e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0ae1545e9ee0227e9123bac8e42efb",
+    "url": "/Career-Builder/index.html"
+  },
+  {
+    "revision": "9637d46a3bd6a60e310b",
+    "url": "/Career-Builder/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "7f74f9ff8aa9e35c9995",
+    "url": "/Career-Builder/static/css/main.bf04da63.chunk.css"
+  },
+  {
+    "revision": "9637d46a3bd6a60e310b",
+    "url": "/Career-Builder/static/js/2.7bb2a4f5.chunk.js"
+  },
+  {
+    "revision": "7b53ff0ecf3a57b38816cbd58bb98e93",
+    "url": "/Career-Builder/static/js/2.7bb2a4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f74f9ff8aa9e35c9995",
+    "url": "/Career-Builder/static/js/main.8dddbaeb.chunk.js"
+  },
+  {
+    "revision": "869ed1feb54993fb86b0",
+    "url": "/Career-Builder/static/js/runtime-main.e1174f45.js"
+  },
+  {
+    "revision": "f5fcaea8cb70bb7e133a6ed8a47fbc4f",
+    "url": "/Career-Builder/static/media/admin-carousel.f5fcaea8.webp"
+  },
+  {
+    "revision": "5c532c2c4b3c9d2b51f3a421acc4265d",
+    "url": "/Career-Builder/static/media/admin-carousel2.5c532c2c.jpeg"
+  },
+  {
+    "revision": "33c2be5b2e014dc379f54e9ad6d00880",
+    "url": "/Career-Builder/static/media/company-carousel.33c2be5b.jpeg"
+  },
+  {
+    "revision": "4ad574ff5d6ba3c48c3fbe5f0d92fa32",
+    "url": "/Career-Builder/static/media/company-form.4ad574ff.jpg"
+  },
+  {
+    "revision": "9d5ce207f1528e632e258ee64104f260",
+    "url": "/Career-Builder/static/media/footer-email-img.9d5ce207.jpg"
+  },
+  {
+    "revision": "9f3e304ef81b755529575a83c91dc26d",
+    "url": "/Career-Builder/static/media/job-carousel.9f3e304e.jpeg"
+  },
+  {
+    "revision": "3c0b42a3bfef654ceb47efbf1706116d",
+    "url": "/Career-Builder/static/media/jobapplicant-form.3c0b42a3.jpg"
+  },
+  {
+    "revision": "66ef6271c1b59f496ac2acdc7d5d2418",
+    "url": "/Career-Builder/static/media/team-carousel.66ef6271.jpeg"
+  }
+]);
